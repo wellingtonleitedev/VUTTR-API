bfd2c10e8146ca96609ff6842e8f94c8d5c8c457
Wellington Leite <wellingtonleitee.s@gmail.com>
2020-01-05 23:31:49 -0300

    chore:troca banco mysql para postgres

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,10 @@
 module.exports = {
-  dialect: 'mysql',
-  username: 'root',
-  host: '127.0.0.1',
-  port: '3306',
-  database: 'vuttr',
+  dialect: process.env.POSTGRES_DIALECT,
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DATABASE,
   define: {
     timestamps: true,
     underscored: true,
